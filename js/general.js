88890b3d5f0e3c4164ab46ca8f35cc00f2f7393c
Alexandr Tovmach <alexandrtovmach@gmail.com>
2017-07-15 19:12:03 +0300

    fix commit
    - refactoring js code
    - add button to clear local storage
    - re-design to Light theme

--- js/general.js
+++ js/general.js
@@ -2,186 +2,181 @@
  * Created by Alexandr on 07.07.2017.
  */
 function init() {
-    document.getElementsByClassName('c_aside')[0].addEventListener('click', showSideBar);
-	document.addEventListener('click', function (event) {
-		if ((!phaseSmallMenu)||(event.target == document.getElementsByClassName('h_panelBar')[0])||
-		(event.target == document.getElementsByTagName('INPUT')[0])||
-		(event.target == document.getElementsByClassName('h_search')[0])) {return}
-		document.getElementsByClassName('h_panel')[0].style.display = 'none';
-		phaseSmallMenu = 0;
-	});
-	document.getElementsByClassName('h_search')[0].getElementsByTagName('input')[0].addEventListener('keyup', function(event) {search(event)});		
-	//add event listener for infinity scroll
+    //adding function for clear localStorage and reload page
+    document.getElementById('clearButton').addEventListener('click', function () {window.localStorage.clear(); location.reload()});
+    //adding keyup function to start search
+    document.getElementsByClassName('h_search')[0].getElementsByTagName('input')[0].addEventListener('keyup', function(event) {search(event)});
+    //add event listener for infinity scroll
     document.onscroll = function () {
-    	var heightPage = (document.documentElement)? document.documentElement: document.body;
+        var heightPage = (document.documentElement)? document.documentElement: document.body;
         if(window.pageYOffset > (heightPage.scrollHeight - heightPage.clientHeight - 100)) {
             createPostInDOM(allPost);
         }
     }
 }
 
-var phaseSideBar = 0;
-var phaseSmallMenu = 0;
-
-function showSideBar() {
-    var element = document.getElementsByClassName('c_aside')[0]
-    if (!phaseSideBar) {
-        element.firstElementChild.style.left = '0px';
-        phaseSideBar = 1;
-    } else {
-        element.firstElementChild.style.left = '';
-        phaseSideBar = 0;
-    }
-}
-function smallMenuShow() {
-	var elem = document.getElementsByClassName('h_panelBar')[0];
-	elem.previousElementSibling.style.display = 'block';
-	elem.previousElementSibling.style.top = elem.getBoundingClientRect().top + 40 + 5 + 'px';
-	elem.previousElementSibling.style.right = '5px';
-	phaseSmallMenu = 1;
-}
-
-
-//dislikedPosts need in global for "onclick" function of delete Post 
-var dislikedPosts = (localStorage.getItem('dislikedPosts'))? localStorage.getItem('dislikedPosts').split(','): [];
-var allPost = [];
+//dislikedPosts need in global for "onclick" function of delete Post
+var dislikedPosts = (localStorage.getItem('dislikedPosts'))? localStorage.getItem('dislikedPosts').split(','): [],
+    allPost = [],
+    countPosts = 0,
+    json,
+    arrLikesTags = [],
+    likedPosts = [],
+    notLikedPosts = [];
 
 //general function of load/parse/sort/show/delete data
-function loadContent(nameFunc) {
-	if (nameFunc == 'showPost') {}
-	var json;
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'https://api.myjson.com/bins/152f9j');
-	xhr.send();
-	xhr.onload = function () {
+function loadContent() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.myjson.com/bins/152f9j');
+    xhr.send();
+    xhr.onload = function () {
         if (xhr.status == 200) {
             json = JSON.parse(xhr.responseText).data;
-			showPost();
+            showPost();
         } else {
-			alert('Sorry, but server not response\n' + xhr.status + ' : ' + xhr.statusText)
-		}
-    }
-	
-	//check localStorage to liked tags
-	function checkLocalStorage() {
-		if(!localStorage.getItem('tagsOfPost')) {
-			return false;
-		} else {
-			return localStorage.getItem('tagsOfPost').split(',');
-		}
-	}
-	
-	//select Post for showing
-	function showPost() {
-		var arrLikesTags = [],
-			likedPosts = [],
-			notLikedPosts = [];
-		if (!(checkLocalStorage())) {
-			selectTags(json);
-		} else {
-            document.forms.selectTag.className = 'hidden';
-			arrLikesTags = checkLocalStorage();
-			//set only liked post to likedPosts from json
-			strt: for (var key in json) {
-				for (var i = 0; i < json[key].tags.length; i++) {
-					for (var e = 0; e < arrLikesTags.length; e++) {
-						if (arrLikesTags[e] == json[key].tags[i]) {
-							if (checkToDislike(json[key])) {
-								likedPosts.push(json[key]);
-								continue strt;
-							}
-						} else {
-							if (checkToDislike(json[key])) {
-								notLikedPosts.push(json[key]);
-								continue strt;
-							}
-						}
-					}
-				}
-			}
-			
-			likedPosts.sort(sortToDate);
-			notLikedPosts.sort(sortToDate);
-			//combine post arrays to one allPost
-			allPost = likedPosts.concat(notLikedPosts);
-			createPostInDOM(allPost);
-			
-			function checkToDislike(obj) {
-				var res = true;
-				for (var y = 0; y < dislikedPosts.length; y++) {
-					if (+Date.parse(obj.createdAt) == +dislikedPosts[y]) {
-						res = false;
-					}
-				}
-				return res;
-			}
-			
-			//sort function
-			function sortToDate(a, b) {
-                if (Date.parse(a.createdAt) < Date.parse(b.createdAt)) {
-                    return 1;
-                }
-                if (Date.parse(a.createdAt) > Date.parse(a.createdAt)) {
-                    return -1
+            alert('Sorry, but server not response\n' + xhr.status + ' : ' + xhr.statusText)
+        }
+    };
+}
+
+//select Post for showing
+function showPost() {
+    if (!(checkLocalStorage())) {
+        selectTags(json);
+    } else {
+        document.forms.selectTag.className = 'hidden';
+        arrLikesTags = checkLocalStorage();
+        //set only liked post to likedPosts from json
+        strt: for (var key in json) {
+            for (var i = 0; i < json[key].tags.length; i++) {
+                for (var e = 0; e < arrLikesTags.length; e++) {
+                    if (arrLikesTags[e] == json[key].tags[i]) {
+                        if (checkToDislike(json[key])) {
+                            likedPosts.push(json[key]);
+                            continue strt;
+                        }
+                    } else {
+                        if (checkToDislike(json[key])) {
+                            notLikedPosts.push(json[key]);
+                            continue strt;
+                        }
+                    }
                 }
             }
-		}
-		
-		//show form to select likes tags
-		function selectTags(json) {
-			var formSelect = document.forms.selectTag;
-			formSelect.className = 'selectTag';
-			
-			//push all tags to uniqeTags
-			var uniqeTags = [];
-			for (var key in json) {
-				for (var i = 0; i < json[key].tags.length; i++) {
-					uniqeTags.push(json[key].tags[i])
-				}
-			}
-			//remove duplicate from uniqeTags
-			uniqeTags = unique(uniqeTags)
-			//create checkbox
-			for (var w = 0; w < uniqeTags.length; w++) {
-				var checkboxEl = document.createElement('input'),
-					labelEl = document.createElement('label');
-				checkboxEl.type = 'checkbox';
-				checkboxEl.value = uniqeTags[w];
-				labelEl.innerHTML = uniqeTags[w];
-				labelEl.prepend(checkboxEl);
-				formSelect.prepend(labelEl);
-			}
-			formSelect.classList.remove('hidden');
-			
-			//onsubmit form preventDefault and get checked tags to arrLikesTags and add to localStorage
-			formSelect.onsubmit = function (event) {
-				event.preventDefault();
-				var arrInput = formSelect.getElementsByTagName('input');
-				var cacheForEmptySelect = [];
-				for (var i = 0; i < arrInput.length; i++) {
-					if (arrInput[i].checked) {
-						arrLikesTags.push(arrInput[i].value)
-					}
-					cacheForEmptySelect.push(arrInput[i].value)
-				}
-				formSelect.className = 'hidden';
-				if (arrLikesTags.length == 0) {arrLikesTags = cacheForEmptySelect}
-				localStorage.setItem('tagsOfPost', arrLikesTags);
-				showPost();
-			};
-		}
-	}
-	
-	//function for remove duplicate
-	function unique(arr) {
-		var obj = {};
-	
-		for (var i = 0; i < arr.length; i++) {
-			var str = arr[i];
-			obj[str] = true;
-		}
-		return Object.keys(obj);
-	}
+        }
+
+        likedPosts.sort(sortToDate);
+        notLikedPosts.sort(sortToDate);
+
+        //combine post arrays to one allPost
+        allPost = likedPosts.concat(notLikedPosts);
+
+        createPostInDOM(allPost);
+    }
+}
+
+
+//check localStorage to liked tags
+function checkLocalStorage() {
+    if(!localStorage.getItem('tagsOfPost')) {
+        return false;
+    } else {
+        return localStorage.getItem('tagsOfPost').split(',');
+    }
+}
+
+
+//show form to select likes tags
+function selectTags(json) {
+    var formSelect = document.forms.selectTag;
+    formSelect.className = 'selectTag';
+
+    //push all tags to uniqeTags
+    var uniqeTags = [];
+    for (var key in json) {
+        for (var i = 0; i < json[key].tags.length; i++) {
+            uniqeTags.push(json[key].tags[i])
+        }
+    }
+    //remove duplicate from uniqeTags
+    uniqeTags = unique(uniqeTags)
+    //create checkbox
+    for (var w = 0; w < uniqeTags.length; w++) {
+        var checkboxEl = document.createElement('input'),
+            labelEl = document.createElement('label');
+        checkboxEl.type = 'checkbox';
+        checkboxEl.value = uniqeTags[w];
+        labelEl.innerHTML = uniqeTags[w];
+        labelEl.prepend(checkboxEl);
+        formSelect.prepend(labelEl);
+    }
+    formSelect.classList.remove('hidden');
+
+    //onsubmit form preventDefault and get checked tags to arrLikesTags and add to localStorage
+    formSelect.onsubmit = function (event) {
+        event.preventDefault();
+        var arrInput = formSelect.getElementsByTagName('input');
+        var cacheForEmptySelect = [];
+        for (var i = 0; i < arrInput.length; i++) {
+            if (arrInput[i].checked) {
+                arrLikesTags.push(arrInput[i].value)
+            }
+            cacheForEmptySelect.push(arrInput[i].value)
+        }
+        formSelect.className = 'hidden';
+        if (arrLikesTags.length == 0) {arrLikesTags = cacheForEmptySelect}
+        localStorage.setItem('tagsOfPost', arrLikesTags);
+        showPost();
+    };
+}
+
+
+
+//function add post to page
+function createPostInDOM(array, a) {
+    //show in DOM all sorted liked post
+    var cach = (a !== undefined)? a: countPosts;
+    for (countPosts; (countPosts < (cach + 10)&&(countPosts < array.length)); countPosts++) {
+        var b = new Post(array[countPosts]);
+        document.getElementsByClassName('c_article')[0].appendChild(b);
+    }
+
+    //create side nav menu
+    contCreate()
+}
+
+
+
+//checking to dislike function
+function checkToDislike(obj) {
+    var res = true;
+    for (var y = 0; y < dislikedPosts.length; y++) {
+        if (+Date.parse(obj.createdAt) == +dislikedPosts[y]) {
+            res = false;
+        }
+    }
+    return res;
+}
+
+//sort function
+function sortToDate(a, b) {
+    if (Date.parse(a.createdAt) < Date.parse(b.createdAt)) {
+        return 1;
+    }
+    if (Date.parse(a.createdAt) > Date.parse(a.createdAt)) {
+        return -1
+    }
+}
+
+//function for remove duplicate
+function unique(arr) {
+    var obj = {};
+
+    for (var i = 0; i < arr.length; i++) {
+        var str = arr[i];
+        obj[str] = true;
+    }
+    return Object.keys(obj);
 }
 
 //constructor of Post
@@ -227,21 +222,8 @@ function Post(obj) {
 	return postDiv
 }
 
-//function add post to page
-var countPosts = 0;
-function createPostInDOM(array, a) {
-	
-    //show in DOM all sorted liked post
-    var cach = (a !== undefined)? a: countPosts;
-    for (countPosts; (countPosts < (cach + 10)&&(countPosts < array.length)); countPosts++) {
-        var b = new Post(array[countPosts]);
-        document.getElementsByClassName('c_article')[0].appendChild(b);
-    }
-	
-    //create side nav menu
-    contCreate()
-}
 
+//search function
 function search(event) {
 	var searchReq = document.getElementsByClassName('h_search')[0].getElementsByTagName('input')[0].value
 	if ((!searchReq)&&(event.keyCode != 8)&&(event.keyCode != 46)) {
